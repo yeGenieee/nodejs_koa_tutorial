9840eab086e73381dfd7f87ada794d2f0d155746
YejinChoi <cyj95428@naver.com>
2018-04-27 15:28:30 +0900

    use router

--- src/index.js
+++ src/index.js
@@ -12,8 +12,25 @@ router.get('/', (ctx,next) => {
    ctx.body = 'Home';
 });
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+router.get('/about', (ctx,next) => {
+    ctx.body = 'Introduction';
+});
+
+router.get('/about/:name', (ctx, next) => {
+    const { name } = ctx.params; // 라우트 경로에서 :파라미터명으로 정의된 값이 ctx.params 안에 설정됨
+    ctx.body = 'About ' + name;
+});
+
+router.get('/post', (ctx, next) => {
+   const { id } = ctx.request.query;
+   if (id) {
+       ctx.body = 'post # '+ id;
+   } else {
+       ctx.body = 'No post id';
+   }
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
    console.log('koa server is listening to port 4000');
