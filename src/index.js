d82f94d6d0646373bc4ec454f4a0c41d47779f86
YejinChoi <cyj95428@naver.com>
2018-04-27 15:14:52 +0900

    koa router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
 
-app.use(ctx => {
-   ctx.body = 'Hello Koa';
+const app = new Koa();
+const router = new Router();
+
+
+// ctx =>: 미들웨어, 웹 요청, 응답에 대한 정보를 가지고 있음
+// next : 다음 미들웨어를 실행시키는 함수
+
+router.get('/', (ctx,next) => {
+   ctx.body = 'Home';
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(4000, () => {
    console.log('koa server is listening to port 4000');
